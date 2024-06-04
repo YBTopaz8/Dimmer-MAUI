0cf3cf6364c5544a6740a9da3903c2fe7a0e9a8e
Yvan Brunel <8brunel@gmail.com>
2024-06-03 20:43:19 -0400

    More updates

--- MauiAudio/obj/Debug/net8.0-android/MauiAudio.AssemblyInfo.cs
+++ MauiAudio/obj/Debug/net8.0-android/MauiAudio.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("MauiAudio-Cross platform audio plugin for MAUI")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.2.2.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.2.2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.2.2+2c6aa81ac66433641a134da3944c51388494a079")]
 [assembly: System.Reflection.AssemblyProductAttribute("MauiAudio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MauiAudio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.2.2.0")]
