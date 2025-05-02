2b271d4d547503efa31d64181b0af9e74e70d228
Yvan Brunel <41630728+YBTopaz8@users.noreply.github.com>
2025-05-02 08:45:10 -0400

    Add IStatisticsStateService interface and StatisticsMgtFlow class
    - Introduced `IStatisticsStateService` interface in `Dimmer.Interfaces`, implementing `IDisposable`.
    - Added `StatisticsMgtFlow` class in `Dimmer.Orchestration`, also implementing `IDisposable`.
    - `StatisticsMgtFlow` constructor accepts multiple repository and service dependencies for managing statistics flow.
    
    By Yvan Brunel

---
+++ Dimmer/Dimmer/Dimmer/Orchestration/StatisticsMgtFlow.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dimmer.Orchestration;
+public class StatisticsMgtFlow : BaseAppFlow, IDisposable
+{
+    public StatisticsMgtFlow(IPlayerStateService state, IRepository<SongModel> songRepo, IRepository<GenreModel> genreRepo, IRepository<AlbumArtistGenreSongLink> aagslRepo, IRepository<PlayDateAndCompletionStateSongLink> pdlRepo, IRepository<PlaylistModel> playlistRepo, IRepository<ArtistModel> artistRepo, IRepository<AlbumModel> albumRepo, ISettingsService settings, IFolderMonitorService folderMonitor, IMapper mapper) : base(state, songRepo, genreRepo, aagslRepo, pdlRepo, playlistRepo, artistRepo, albumRepo, settings, folderMonitor, mapper)
+    {
+    }
+}
