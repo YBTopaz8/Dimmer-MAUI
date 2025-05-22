88d46f381e23c21946a0c5076d20169a1894d932
Yvan Brunel <41630728+YBTopaz8@users.noreply.github.com>
2025-05-22 05:44:41 -0400

    Add LabelValue class and PlotStats utility methods
    - Introduced `LabelValue` class for encapsulating label-value pairs, enhancing data representation.
    - Created `PlotStats` static class with methods for generating detailed statistics on song plays, skips, and other metrics.
    - Utilized LINQ for efficient data manipulation, enabling comprehensive insights into song and play event data.
    
    By Yvan Brunel

---
+++ Dimmer/Dimmer/Dimmer/Data/ModelView/LabelValue.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dimmer.Data.ModelView;
+public class LabelValue
+{
+    public string Label { get; set; }
+    public double Value { get; set; }
+    public LabelValue(string label, double value) { Label = label; Value = value; }
+}
