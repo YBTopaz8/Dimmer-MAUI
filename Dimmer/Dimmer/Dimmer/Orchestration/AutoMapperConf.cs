f91b03f69463961f0461b90c79d3fd938494b9ed
Yvan Brunel <8brunel@gmail.com>
2025-07-19 10:31:07 -0400

    Update AutoMapper mappings for SongModel properties
    Revised the mapping configuration in `AutoMapperConf` to use
    `ArtistName` and `AlbumName` instead of `Artist.Name` and
    `Album.Name` for the `SongModel` to `SongModelView` mapping.
    This change ensures the correct property names are utilized
    following recent updates to the source model.

--- Dimmer/Dimmer/Dimmer/Orchestration/AutoMapperConf.cs
+++ Dimmer/Dimmer/Dimmer/Orchestration/AutoMapperConf.cs
@@ -22,8 +22,8 @@ public static class AutoMapperConf
                 // These are explicit to prevent threading issues and silent failures.
 
                 cfg.CreateMap<SongModel, SongModelView>()
-                    .ForMember(dest => dest.ArtistName, opt => opt.MapFrom(src => src.Artist != null ? src.Artist.Name : "Unknown Artist"))
-                    .ForMember(dest => dest.AlbumName, opt => opt.MapFrom(src => src.Album != null ? src.Album.Name : "Unknown Album"))
+                    .ForMember(dest => dest.ArtistName, opt => opt.MapFrom(src => src.Artist != null ? src.ArtistName : "Unknown Artist"))
+                    .ForMember(dest => dest.AlbumName, opt => opt.MapFrom(src => src.Album != null ? src.AlbumName : "Unknown Album"))
                     .ForMember(dest => dest.GenreName, opt => opt.MapFrom(src => src.Genre != null ? src.Genre.Name : "Unknown Genre"))
                     .ForMember(dest => dest.Album, opt => opt.Ignore()) // Must ignore RealmObject properties
                     .ForMember(dest => dest.Genre, opt => opt.Ignore())
