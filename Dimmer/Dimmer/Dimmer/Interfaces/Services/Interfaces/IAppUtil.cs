b6a52b1eaa06a4c98bb4b1ec5146e25ba0b752c1
Yvan Brunel <8brunel@gmail.com>
2025-07-20 00:41:22 -0400

    Refactor HomePage layout and remove song criteria classes
    Updated the layout in `HomePage.xaml` to improve organization by replacing `HorizontalStackLayout` with a `Grid`, modifying `RowDefinitions` and `ColumnDefinitions`, and adding new UI elements for better user interaction.
    
    Removed the `IViewCriteria` interface and `QueryBasedSongCriteria` class from `IAppUtil.cs`, indicating a significant change in how song filtering and sorting criteria are managed within the application.

--- Dimmer/Dimmer/Dimmer/Interfaces/Services/Interfaces/IAppUtil.cs
+++ Dimmer/Dimmer/Dimmer/Interfaces/Services/Interfaces/IAppUtil.cs
@@ -7,27 +7,3 @@ public interface IAppUtil
     public Shell GetShell();
     public Window LoadWindow();
 }
-public interface IViewCriteria
-{
-    Func<SongModelView, bool> Filter { get; }
-    IComparer<SongModelView> Comparer { get; }
-    LimiterClause? Limiter { get; }
-}
-public class QueryBasedSongCriteria : IViewCriteria
-{
-    public Func<SongModelView, bool> Filter { get; }
-    public IComparer<SongModelView> Comparer { get; }
-    public LimiterClause? Limiter { get; }
-    public string Query { get; }
-
-    public QueryBasedSongCriteria(string query)
-    {
-        Query = query;
-
-        var metaParser = new MetaParser(query);
-
-        Filter = metaParser.CreateMasterPredicate();
-        Comparer = metaParser.CreateSortComparer();
-        Limiter = metaParser.CreateLimiterClause();
-    }
-}
