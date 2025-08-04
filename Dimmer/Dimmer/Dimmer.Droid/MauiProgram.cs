16fa8c5436d9504463ddd31f144fdbf642eae380
Yvan Brunel <8brunel@gmail.com>
2025-08-04 03:31:24 -0400

    Update AndroidManifest and project configuration
    - Enhanced `AndroidManifest.xml` with new attributes for `MainActivity` and `ExoPlayerService`, including intent filters and updated permissions.
    - Modified `Dimmer.Droid.csproj` to add properties like `OutputType`, `PublishAot`, and `TrimMode`, while updating package references and adding Media3 dependencies.
    - Updated `MauiProgram.cs` to configure app actions for audio playback.
    - Adjusted project structure with new folders and linked resources for better management.
    - Created a backup of the `Dimmer.Droid.csproj` file to preserve the previous state.

--- Dimmer/Dimmer/Dimmer.Droid/MauiProgram.cs
+++ Dimmer/Dimmer/Dimmer.Droid/MauiProgram.cs
@@ -10,7 +10,7 @@ public static class MauiProgram
     {
         var builder = MauiApp.CreateBuilder();
         ThemeManager.UseAndroidSystemColor = true;
-
+        
         builder
               .ConfigureEssentials(essentials =>
               {
